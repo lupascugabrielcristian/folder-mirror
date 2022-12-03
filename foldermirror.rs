92119d16fc6177535d96b70a41a537cffad0180c
BoukabousVisanzay <9123f887-43d7-46e7-b59f-5bc61b8dbb73@anonaddy.me>
2022-12-03 14:37:08 +0200

    Scrie corect fisierul xml

--- foldermirror.rs
+++ foldermirror.rs
@@ -4,15 +4,21 @@ use std::{fs,str};
 use std::path::Path;
 use std::env;
 use std::any::type_name;
-use quick_xml::events::{Event, BytesStart, BytesEnd, BytesText};
+use quick_xml::events::{Event, BytesStart, BytesEnd};
 use quick_xml::writer::Writer;
-use std::io::{Cursor};
+
 
 fn main() {
     // TODO
     // Sa verific daca am un argument
 
     let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        println!("Pass the location of the root folder as first parameter");
+        return;
+    }
+
     let main_dir = &args[1];
 
     // Verific daca exista directorul
@@ -20,9 +26,6 @@ fn main() {
         println!("Cannot find specified path");
         return;
     }
-
-    // Creez xml writer
-    let mut writer = Writer::new(Cursor::new(Vec::new()));
 
 //  Model de scris cod xml
 //    // creez un element de tip <my_elem> si ii pun in buffer inceputul
@@ -49,14 +52,23 @@ fn main() {
     // Creates single element
     //writer.create_element("my_elem").write_empty();
 
+    let mut file_count = 0;
+    let mut dir_count = 0;
+
+    // Creez xml writer
+    let mut buffer = Vec::<u8>::new();
+    let mut writer_indent = Writer::new_with_indent(&mut buffer, b' ', 4);
+
+
     let p = Path::new(main_dir);
-    print_dir_contents(p, &mut writer);
+    print_dir_contents(p, &mut writer_indent, &mut file_count, &mut dir_count );
 
+    println!("{} files", file_count);
+    println!("{} directories", dir_count);
 
-    // extrag textul din bufferul writer-ului
-    let result = writer.into_inner().into_inner();
     // Vec<utf8> in str
-    let str_result = str::from_utf8(&result);
+    //let str_result = str::from_utf8(&result);
+    let str_result = str::from_utf8(&buffer);
     let output_path = Path::new("output.xml");
     let write_result = fs::write(&output_path, str_result.unwrap());
     match write_result {
@@ -71,21 +83,18 @@ fn _print_type<T>(_: &T) {
     println!("{}", type_name::<T>());
 }
 
-fn print_dir_contents(path: &Path, writer: &mut Writer<Cursor<Vec<u8>>>) {
+fn print_dir_contents(path: &Path, writer: &mut Writer<&mut Vec<u8>>, file_count: &mut i32, dir_count: &mut i32) {
 
     if path.is_dir() {
+        *dir_count += 1;
+
         // dir_name este str
         let dir_name = path.file_name().unwrap()
                 .to_str().unwrap();
-        // Scriu un element gol cu numele directorului
-        //match writer.create_element(dir_name).write_empty() {
-        //    Ok(_) => (),
-        //    Err(_) => return,
-        //};
 
         // creez un element de tip <my_elem> si ii pun in buffer inceputul
-        let elem = BytesStart::new(dir_name);
-        // la elem adaug o proprietate si devine <my_elem my-key="some value" >
+        let mut elem = BytesStart::new(dir_name);
+        elem.push_attribute(("type", "directory"));
         match writer.write_event(Event::Start(elem)) {
             Ok(_) => (),
             Err(_) => return,
@@ -105,13 +114,15 @@ fn print_dir_contents(path: &Path, writer: &mut Writer<Cursor<Vec<u8>>>) {
             let file_name = &p.as_path().file_name().unwrap()
                 .to_str().unwrap();
 
+            // Skip certain folder names
             if file_name.contains("node_modules") ||
                 file_name.contains(".git") || 
-                file_name.contains("__pycache__"){
+                file_name.contains("__pycache__") ||
+                file_name.contains("build") {
                 continue
             }
 
-            print_dir_contents( &p.as_path(), writer );
+            print_dir_contents( &p.as_path(), writer, file_count, dir_count );
         }
 
         // pun in buffer sfarsitul unui element
@@ -122,6 +133,8 @@ fn print_dir_contents(path: &Path, writer: &mut Writer<Cursor<Vec<u8>>>) {
         }
 
     } else {
+        *file_count += 1;
+
         let file_name = &path.file_name().unwrap()
                 .to_str().unwrap();
 
